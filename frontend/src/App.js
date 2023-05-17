6a62d11f57d08753fef95da43746106d6d4f69a3
Priyanwada Devi <87337107+IT21061066@users.noreply.github.com>
2023-05-17 10:59:55 +0530

    edit app.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+ 
+// We use Route in order to define the different routes of our application
+import { Route, Routes } from "react-router-dom";
+ 
+// We import all the components we need in our app
+// import Navbar from "./Components/Service/create";
+import RecordList from "./Components/Service/recordList";
+import Edit from "./Components/Service/edit";
+import Create from "./Components/Service/create";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import ServiceDetails from "./Components/Service/ServiceDetails";
+import AddService from "./Components/Service/AddService";
+import ServiceDash from "./Components/Service/ServiceDash";
+import DonatePage from "./Components/Donation/DonatePage";
+import AddBasket from "./Components/Donation/AddBasket"
+import AddForm from "./Components/Donation/AddForm"
 
+ 
+const App = () => {
+ return (
+   <div>
+     {/* <Navbar /> */}
+     <Routes>
+       {/* <Route exact path="/" element={<RecordList />} />
+       <Route path="/edit/:id" element={<Edit />} />
+       <Route path="/create" element={<Create />} /> */}
+       <Route path="/" element={<DonatePage/>}/>
+       <Route path="/add" element={<AddBasket/>}/>
+       <Route path="/addForm" element={<AddForm/>}/>
+
+       <Route path="/sdetails" element={<ServiceDetails/>}/>
+       <Route path="/addService" element={<AddService/>}/>
+       <Route path="/serviceDash" element={<ServiceDash/>}/>
+     </Routes>
+   </div>
+ );
+};
+ 
 export default App;
