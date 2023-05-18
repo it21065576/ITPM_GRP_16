dfaf050b730547f96e2a264e76a08985b76b8017
HasinduBandara <87415515+it21047138@users.noreply.github.com>
2023-05-19 03:06:15 +0530

    db link added commit

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,51 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const config = require("config");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const dotenv = require("dotenv");
-dotenv.config();
-
-
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+require("dotenv").config();
 const app = express();
+const fileUpload = require("express-fileupload")
 
-
-
-// Body parser middleware
-app.use(bodyParser.json());
+app.use("/Assets", express.static(__dirname + "/Assets"))
+const port = process.env.PORT || 4000;
+const URL = process.env.MONGO_URL;
 app.use(cors());
+app.use(express.json())
+app.use(fileUpload());
 
-app.use(
-    express.urlencoded({
-        extended: false,
-    })
-);
+mongoose.connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
-//route middleware
+//DB connection
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("MongoDB connected");
+})
+
+app.listen(port, () => {
+    console.log("PORT conneted on " + port);
+})
+
+//plant route
+const plantRoute = require("./Routes/PlantRoutes");
+app.use("/api/products", plantRoute);
+
+//machine route
+const machineRoute = require("./Routes/MchineRoute")
+app.use("/api/machines", machineRoute)
+
+//fetilizer route
+const fertilizerRoute = require("./Routes/FertilizerRoute");
+app.use("/api/fertilizers", fertilizerRoute)
+
+//Blog route
+const BlogRouter = require("./Routes/BlogRoute");
+app.use("/api/Blogs", BlogRouter);
+
+//user managing routes
+const UserManagementRoutes = require("./Controllers/UserController");
+app.use("/api/users", UserManagementRoutes);
 
 
-
-app.use(express.json());
-// DB Config
-const db = config.get("mongoURI");
-// Connect to MongoDB
-mongoose
-    .connect(db, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log("MongoDB successfully connected"))
-    .catch((err) => console.log(err));
-
-const port = process.env.PORT || 8002;
-app.listen(port, () => console.log(`Server up and running on port ${port} !`));
