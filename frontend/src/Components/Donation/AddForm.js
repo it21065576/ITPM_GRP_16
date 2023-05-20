5f0c1f6f82a72a29173dece7c849049af01dd2d2
Priyanwada Devi <87337107+IT21061066@users.noreply.github.com>
2023-05-20 07:25:45 +0530

    edit addfrom.js

--- frontend/src/Components/Donation/AddForm.js
+++ frontend/src/Components/Donation/AddForm.js
@@ -3,18 +3,24 @@ import axios from "axios";
 import { useParams} from "react-router-dom";
 import Footer from '../Footer';
 import Navbar from '../Navbar';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import "../../Styles/addDonarForm.css";
 
 
 
 
-export default function Allprojects() {
+export default function AddForm() {
 
-   const [projectName, setproname] = useState([]);
+   const [fName, setfName] = useState([]);
    //const [photo, setphoto] = useState([]);
-   const [time, settime] = useState([]);
-   const [location, setlocation] = useState([]);
-   const [description, setdescription] = useState([]);
-   const [fileUpload, setFileUpload] = useState();
+   const [lName, setlName] = useState([]);
+   const [address, setaddress] = useState([]);
+   const [phone, setphone] = useState([]);
+   const [email, setemail] = useState();
+   const [amount, setamount] = useState();
 
 
   //page refresh function
@@ -30,113 +36,168 @@ export default function Allprojects() {
     s.preventDefault();
 
     //Creating object
-    const newproject ={
-        projectName,
-        
-        time,
-        location,
-        description
+    const addForm ={
+        fName,
+        lName,
+        address,
+        phone,
+        email,
+        amount
        
     }
 
-    const imageSave = new FormData();
-    imageSave.append("project", fileUpload);
 
-    axios.post("http://localhost:8080/project/save", newproject).then(()=>{
-      axios
-        .post(`http://localhost:8080/upload/project/${projectName}`, imageSave)
-        .then((res) => {
-          alert("project successfully added");
-        });
-    });
+    axios.post("http://localhost:8002/donation/save", addForm).then(()=>{
 
-    
-    // //passing data to the DB
-    // axios.post("http://localhost:8080/project/save", newproject).then(()=>{
-      
+      alert("Details successfully added",refreshPage());
+      //console.log(newAppointment);
 
-    //   alert("project  is successful",refreshPage());
-    //   console.log(newproject);
+    }).catch((err)=>{
 
-    // }).catch((err)=>{
+      alert("Error: Details not added");
+      console.log(err);
 
-    //   alert("Error: project not added");
-    //   console.log(err);
-
-    // })
+    })
 
   }
 
-
-
-
-       
-    
+  
 
   return (
+    
+  <>
+        {/* <Navbar /><center>
+          <form className="addform" onSubmit={sendData}>
+          <Row className="mb-3">
+          <Form.Group as={Col} controlId="fName">
+          <Form.Label>First Name</Form.Label>
+          <Form.Control type="fName" placeholder="Enter First Name" onChange={(event)=>{
+                  setfName(event.target.value);
+              }} required/> 
+        </Form.Group>
+       
 
-    <div className="main-container">
+        <Form.Group as={Col} controlId="lName">
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control type="lName" placeholder="Enter Last Name"  onChange={(event)=>{
+                  setlName(event.target.value);
+              }} required/>
+        </Form.Group>
+      </Row>
+          
+      <Form.Group className="mb-3" controlId="address">
+        <Form.Label>Address</Form.Label>
+        <Form.Control placeholder=" Enter Address" onChange={(event)=>{
+                  setaddress(event.target.value);
+              }} required/> 
+      </Form.Group> 
+      <Row className="mb-3">
+      <Form.Group className="mb-3" controlId="phone">
+        <Form.Label>Phone Number</Form.Label>
+        <Form.Control placeholder=" Enter Phone Number" onChange={(event)=>{
+                  setphone(event.target.value);
+              }} required/>
+      </Form.Group> 
+
+      <Form.Group className="mb-3" controlId="email">
+        <Form.Label>Email Address</Form.Label>
+        <Form.Control placeholder=" Enter Email Address" onChange={(event)=>{
+                  setemail(event.target.value);
+              }} required/>
+      </Form.Group> 
+      </Row>
+
+          </form>
+
+              <input  type="submit" id="saveBtn"  value="SUBMIT" class="btn btn-primary"></input>
+              <br></br>
+              <br></br>
+
+
+        
+       
+        </center>
+        <Footer/> */}
+        <div className="main-container">
 
       
 
-        <Navbar />
-        <div className="body-container clearfix">
-        <div className="n2"><b>Create New project</b></div>
-        <form className="createform" onSubmit={sendData}>   
-        <div className='projectName-container'>
-              <label for="projectName"><b>project Name:</b></label> <br/>
-              <input type="text" id="projectName"  className="values"  name="projectName"  onChange={(event)=>{
-                  setproname(event.target.value);
-              }} required/>
-              </div><br/>
+{/* <NavBar /> */}
+<Navbar/>
+<div className="body-container clearfix">
 
-              
-              {/* <div className='photo-container'>
-              <label for="photo"><b>Choose Images</b></label>  &nbsp;  
-              <input type="file" accept=".png, .jpg, .jpeg" id="photo"   name="photo"  onChange={(event)=>{
-                  setphoto(event.target.value);
-              }} />
-              </div><br/> */}
+<form className="createform" onSubmit={sendData}>  
+<div className="n2"><b><center>
+            <h2>Yes,I want to donate to</h2>
+            <h2>support women and childern!</h2>
+            <p><h6>Your donation will enable Smile Sri Lanka to provide every child and</h6> </p>
+            <p><h6>women with a future and a fair chance.</h6></p></center></b></div> 
+<div className='projectName-container'>
+      <label for="fName"><b>First Name</b></label> <br/>
+      <input type="text" id="fName"  className="values"  name="fName"  
+      onChange={(event)=>{
+          setfName(event.target.value);
+      }} required/>
+      </div><br/>
 
-                <input
-                  className="chooseFile"
-                  type="file"
-                  name="Choose file"
-                  onChange={(e) => {
-                    setFileUpload(e.target.files[0]);
-                  }}
-                />
-              
-              <div className='time-container'>
-              <label for="time"><b>start & End Time:</b></label> <br/>
-              <input type="text" id="time" className="values"    name="time"  onChange={(event)=>{
-                  settime(event.target.value);
-              }} required/>
-              </div><br/>
+      
+      {/* <div className='photo-container'>
+      <label for="photo"><b>Choose Images</b></label>  &nbsp;  
+      <input type="file" accept=".png, .jpg, .jpeg" id="photo"   name="photo"  onChange={(event)=>{
+          setphoto(event.target.value);
+      }} />
+      </div><br/> */}
 
-              
-              <div className='location-container'>
-              <label for="location"><b>Location:</b></label> <br/>  
-              <input type="text" id="location" className="values"   name="location"  onChange={(event)=>{
-                  setlocation(event.target.value);
-              }} required/>
-              </div><br/>
+      
+      
+      <div className='time-container'>
+      <label for="lName"><b>Last Name</b></label> <br/>
+      <input type="text" id="lName" className="values"    name="lName"  onChange={(event)=>{
+          setlName(event.target.value);
+      }} required/>
+      </div><br/>
 
-              <div className='discription-container'>
-              <label for="discription"><b>Discription:</b></label><br/> 
-              <textarea type="textarea" id="discription" className="values1" name="discription"  onChange={(event)=>{
-                  setdescription(event.target.value);
-              }} required/>
-              </div><br/>
-              <input  type="submit" id="saveBtn"  value="SUBMIT"></input>
-              </form>
-        </div>
-        
+      
+      <div className='location-container'>
+      <label for="address"><b>Address:</b></label> <br/>  
+      <input type="text" id="address" className="values"   name="address"  onChange={(event)=>{
+          setaddress(event.target.value);
+      }} required/>
+      </div><br/>
 
-        <Footer/>
+      <div className='discription-container'>
+      <label for="phone"><b>Phone Number:</b></label><br/> 
+      <input type="text" id="phone" className="values" name="phone"  onChange={(event)=>{
+          setphone(event.target.value);
+      }} required/>
+      </div><br/>
+      <div className='discription-container'>
+      <label for="email"><b>Email Address:</b></label><br/> 
+      <input type="text" id="email" className="values" name="email"  onChange={(event)=>{
+          setemail(event.target.value);
+      }} required/>
+      </div><br/>
 
+      <div className='time-container'>
+      <label for="amount"><b>Amount</b></label> <br/>
+      <input type="text" id="amount" className="values"    name="amount"  onChange={(event)=>{
+          setamount(event.target.value);
+      }} required/>
+      </div><br/>
 
-        {/* update and delete */}
+      <a href ="/amount"><input  type="submit" id="saveBtn"  value="SUBMIT"></input></a>
+      </form>
 </div>
+
+
+
+
+
+</div>
+ 
+<Footer/>
+
+    </>
+
     );
 }
