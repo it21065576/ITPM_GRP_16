e4357faf345ff258c760351b5027771a684a19da
Henuka Liyanage <87329830+it21065576@users.noreply.github.com>
2023-05-22 13:33:40 +0530

    edit service dash

--- frontend/src/Components/Service/ServiceDash.js
+++ frontend/src/Components/Service/ServiceDash.js
@@ -1,6 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import swal from 'sweetalert';
+import Swal from 'sweetalert2';
+import { Button, Box, Typography } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import { Link } from 'react-router-dom';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+
+
+
 
 const ServiceDash = () => {
     const [services, setServices] = useState([]);
@@ -19,12 +28,39 @@ const ServiceDash = () => {
     }, []);
 
     const onDelete = (id) => {
-        axios.delete(`http://localhost:8002/service/delete/${id}`)
-            .then((res) => {
-                swal("Deleted Successfully");
+        Swal.fire({
+          title: 'Are you sure?',
+          text: "You won't be able to revert this!",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            axios.delete(`http://localhost:8002/service/delete/${id}`)
+              .then((res) => {
+                Swal.fire({
+                  position: 'center',
+                  icon: 'success',
+                  title: 'Successfully Deleted',
+                  showConfirmButton: false,
+                  timer: 1500
+                });
                 deleteService();
-            })
-    }
+              })
+              .catch((error) => {
+                Swal.fire({
+                  position: 'center',
+                  icon: 'error',
+                  title: 'Failed to delete',
+                  showConfirmButton: false,
+                  timer: 1500
+                });
+              });
+          }
+        });
+      }
 
     const deleteService = () => {
         axios.get("http://localhost:8002/service")
@@ -33,6 +69,7 @@ const ServiceDash = () => {
                 setAllServices(response.data);
             });
     }
+
     function searchfun(e) {
 
         setQuery(e.target.value)
@@ -40,20 +77,15 @@ const ServiceDash = () => {
 
     return (
         <>
-
-            <div className="order_bground" style={{ zIndex: 98, height: "100vh" }} >
-                <br />
-                <div style={{ width: '20%', marginLeft: '80%', marginBlockStart: '2%' }}>
-                    <form className="d-flex">
-                        <input
-                            className="form-control me-2"
-                            type="search"
-                            placeholder="Search"
-                            aria-label="Search"
-                            onChange={searchfun}
-                        />
-                    </form>
-                </div>
+          <div style={{ width: '20%', marginLeft: '80%', marginBlockStart: '2%'}}>
+          <form className="d-flex">
+            <input className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search" onChange={searchfun}>
+            </input>
+          </form>
+        </div>
 
                 <center>
                     <h1>
@@ -62,86 +94,100 @@ const ServiceDash = () => {
                         </span>
                     </h1>
                 </center>
-                <br />
 
                 <div>
-                    <table className="table table-bordered" style={{ width: '100%', marginBlockStart: '-1%' }} >
-                        <thead className="table-info">
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Name</th>
-                                <th scope="col">Email</th>
-                                <th scope="col">Contact Number</th>
-                                <th scope="col">Job Title</th>
-                                <th scope="col">Position</th>
-                                <th scope="col">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {services && services.filter(e =>
-
-
-
-                                e.fName.toLowerCase().includes(serQuery) ||
-                                e.fName.includes(serQuery) ||
-
-                                e.email.toLowerCase().includes(serQuery) ||
-                                e.email.includes(serQuery) ||
-
-                                e.job.toLowerCase().includes(serQuery) ||
-                                e.job.includes(serQuery) ||
-
-                                e.position.toLowerCase().includes(serQuery) ||
-                                e.position.includes(serQuery) ||
-
-                                e.phone.toLowerCase().includes(serQuery) ||
-                                e.phone.includes(serQuery) 
-
-
-                            ).map((service, index) => (
-                                <tr key={index}>
-                                    <th className="table-light" scope="row">{index + 1}</th>
-                                    <td className="table-light">{service.fName}</td>
-                                    <td className="table-light">{service.email}</td>
-                                    <td className="table-light">{service.phone}</td>
-                                    <td class="table-light">{service.job}</td>
-                                    <td class="table-light">{service.position}</td>
-                                    <td class="table-light">
-
-                                        <a class="btn btn-warning" style={{ marginLeft: '-1%', width: '50%' }} href={`/editEmployee/${services._id}`}> <i className="fas fa-edit"></i>Edit</a>
-                                        &nbsp;&nbsp;&nbsp;&nbsp;
-                                        {/* <a class="btn btn-danger" style={{ marginLeft: '50%', width: '50%', marginBlockStart: '-34%' }} onClick={() => this.onDelete(services._id)} ><i className="far fa-trash-alt"></i>Delete</a> */}
-                                        <button className="btn btn-danger" style={{ marginLeft: '50%', width: '50%', marginBlockStart: '-34%' }} onClick={() => onDelete(service._id)}><i className="far fa-trash-alt"></i>Delete</button>
-
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-
-                    <br></br>
-
                     <div>
+                        <table className="table table-bordered" style={{ width: '100%', marginBlockStart: '2%' }}>
+                            <thead className="table-info">
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Contact Number</th>
+                                    <th scope="col">Job Title</th>
+                                    <th scope="col">Position</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {services &&
+                                    services
+                                        .filter((e) => {
+                                            const lowerCaseSerQuery = serQuery.toLowerCase();
+                                            return (
+                                                e.fName.toLowerCase().includes(lowerCaseSerQuery) ||
+                                                e.email.toLowerCase().includes(lowerCaseSerQuery) ||
+                                                e.job.toLowerCase().includes(lowerCaseSerQuery) ||
+                                                e.position.toLowerCase().includes(lowerCaseSerQuery) ||
+                                                e.phone.toLowerCase().includes(lowerCaseSerQuery)
+                                            );
+                                        })
+                                        .map((service, index) => (
+                                            <tr key={index}>
+                                                <th className="table-light" scope="row">{index + 1} </th>
+                                                <td className="table-light">{service.fName}</td>
+                                                <td className="table-light">{service.email}</td>
+                                                <td className="table-light">{service.phone}</td>
+                                                <td className="table-light">{service.job}</td>
+                                                <td className="table-light">{service.position}</td>
+                                                <td className="table-light" style={{ width: '25%' }}>
 
-                        <a style={{ marginLeft: '35%' }} className="btn btn-warning text-dark " href="/addService" >
-                            <i className="fas fa-user-plus"></i>&nbsp;Add New Service Provider
-                        </a>
-                        &nbsp;
-                        <a style={{ marginLeft: '1%' }} className="btn btn-warning text-dark " href="/staff_dash" >
-                            Dash Board
-                        </a>
+                                                    <div className="d-flex justify-content-center">
+                                                        <Button
+                                                            variant="contained"
+                                                            color="warning"
+                                                            startIcon={<EditIcon />}
+                                                            style={{ width: '30%', backgroundColor: 'seagreen' }}
+                                                            href={`/editService/${service._id}`}
+                                                        >
+                                                            Edit
+                                                        </Button>
+
+                                                        <Button
+                                                            variant="contained"
+                                                            color="error"
+                                                            startIcon={<DeleteIcon />}
+                                                            style={{ width: '30%', marginLeft: '10px', backgroundColor:'dark red' }}
+                                                            onClick={() => onDelete(service._id)}
+                                                        >
+                                                            Delete
+                                                        </Button>
+                                                    </div>
+
+                                                </td>
+                                            </tr>
+                                        ))}
+                            </tbody>
+                        </table>
                     </div>
-                    <br></br>
-                    <br></br>
-                </div>
+                    <div style={{ marginBlockStart: '2%'}}>
+                        <Box display="flex" justifyContent="center">
+                            <Button
+                                as={Link}
+                                to="/addService"
+                                variant="warning"
+                                className="text-dark"
+                                style={{ marginLeft: '-1%', boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.25)', backgroundColor: 'ThreeDDarkShadow',padding: '10px 20px' }}
+                            >
+                                <AddCircleIcon sx={{ marginRight: '0.5rem' }} />
+                                Add New Service Provider
+                            </Button>
 
-                {/* </div> */}
-            </div>
+                            <Button
+                                as={Link}
+                                to="/"
+                                variant="warning"
+                                className="text-dark"
+                                style={{ marginLeft: '1%', boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.25)', backgroundColor: 'honeydew',padding: '10px 20px' }}
+                            >
+                                <DashboardIcon sx={{ marginRight: '0.5rem' }} />
+                                Dashboard
+                            </Button>
+                        </Box>
+                    </div>
+                </div>
         </>
     )
-
 }
 
-
 export default ServiceDash;
-
