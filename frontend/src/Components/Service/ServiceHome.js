8bdd50931d88fa2cff0092cc61e9da74ff5ef0e3
Henuka Liyanage <87329830+it21065576@users.noreply.github.com>
2023-05-22 13:33:31 +0530

    add map to service home.js

--- frontend/src/Components/Service/ServiceHome.js
+++ frontend/src/Components/Service/ServiceHome.js
@@ -1,43 +1,69 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Container, Typography, Button } from '@material-ui/core';
+import { Container, Grid, Card, CardContent, CardMedia, Typography, Button } from '@material-ui/core';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const useStyles = makeStyles(theme => ({
-  container: {
-    marginTop: theme.spacing(4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  title: {
-    marginBottom: theme.spacing(2),
-  },
-  button: {
-    marginTop: theme.spacing(2),
-  },
-}));
+import ServiceImage from './Images/service.png';
+import PdfImage from './Images/pdfDown.png';
 
-function HomePage() {
-  const classes = useStyles();
 
+
+
+const StaffManagementDashboard = () => {
   return (
-    <Container className={classes.container} maxWidth="sm">
-      <Typography variant="h4" component="h1" className={classes.title}>
-        Welcome to My App
-      </Typography>
-      <Typography variant="subtitle1" component="p">
-        This is a sample home page built with React and Material-UI.
-      </Typography>
-      <Button
-        variant="contained"
-        color="primary"
-        className={classes.button}
-        href="/serviceDash"
-      >
-        Get Started
-      </Button>
+    <Container maxWidth="lg">
+      <Grid container spacing={3} justify="center">
+        <Grid item xs={12}>
+          <Typography variant="h1" align="center" component="div">
+            <span className="badge bg-warning text-dark opacity-90 fs-1">Service Management Dashboard</span>
+          </Typography>
+        </Grid>
+
+
+        <Grid item xs={12} sm={6}>
+          <Card style={{ width: "78%", height: "80%", marginBlockStart: "20%", marginLeft: "12%" }}>
+            <CardMedia component="img" src={ServiceImage} alt="Create new Service" />
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                Service Provider List
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Registered Service Provider details in our web site.
+              </Typography>
+              <Button href="/serviceDash" variant="contained" color="primary" style={{ marginBlockStart: "4%" }}>
+                See More
+              </Button>
+            </CardContent>
+          </Card>
+        </Grid>
+
+
+        <Grid item xs={12} sm={6} >
+          <Card style={{ width: "78%", height: "80%", marginBlockStart: "20%", marginLeft: "9%" }}>
+            <CardMedia component="img" src={PdfImage} alt="Service Report" />
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                Service Report
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                These Reports are helping to get data about Service Providers as PDF.
+              </Typography>
+              <Button href="/serviceReport" variant="contained" color="primary">
+                Generate Report
+              </Button>
+            </CardContent>
+          </Card>
+        </Grid>
+
+        <Grid style={{marginBlockStart:"5%", borderStyle:'solid', borderBlockColor:'black'}}>
+        <iframe class="gmap_iframe" width="1200" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=1000&amp;height=400&amp;hl=en&amp;q=child and women protection departments in sri lanka &amp;t=k&amp;z=10&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+        </Grid>
+
+
+
+
+      </Grid>
     </Container>
   );
-}
+};
 
-export default HomePage;
+export default StaffManagementDashboard;
