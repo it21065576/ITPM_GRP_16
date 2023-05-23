aba6ed9ad4c0dbc48d2fcf73ecaf3e72cc357830
Priyanwada Devi <87337107+IT21061066@users.noreply.github.com>
2023-05-23 20:54:06 +0530

    Add files via upload
    add donatiom

---
+++ backend/models/Donation/DonarDetails.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const DonationSchema = new mongoose.Schema({
+  fName: {
+    type: String,
+    required: true,
+  },
+  lName: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+ 
+  phone: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+  },
+
+  amount: {
+    type: String,
+    required: true,
+  },
+ 
+});
+
+module.exports = mongoose.model("DonarDetails", DonationSchema);
